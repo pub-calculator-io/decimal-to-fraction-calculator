fbd56646ae9de4cbdd7e56917a40168dcff380aa
Arseniy Olevskiy <arsidian@gmail.com>
2023-10-24 15:56:19 +0400

    regular commit

--- plugin.php
+++ plugin.php
@@ -10,6 +10,8 @@ License: GPLv2 or later
 Text Domain: ci_decimal_to_fraction_calculator
 */
 
+if (!defined('ABSPATH')) exit;
+
 if (!function_exists('add_shortcode')) return "No direct call for Decimal to Fraction Calculator by Calculator.iO";
 
 function display_ci_decimal_to_fraction_calculator(){
